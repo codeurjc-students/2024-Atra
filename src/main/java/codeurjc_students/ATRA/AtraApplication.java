85462530a8f5bc0590fa7a268050a19cae24bc38
AngelMarquesGarcia <117300926+AngelMarquesGarcia@users.noreply.github.com>
2024-10-17 14:55:22 +0200

    build: initial project structure and config files

---
+++ src/main/java/codeurjc_students/ATRA/AtraApplication.java
@@ -0,0 +1,13 @@
+package codeurjc_students.ATRA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codeurjc_students/ATRA/AtraApplicationTests.java
@@ -0,0 +1,13 @@
+package codeurjc_students.ATRA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
