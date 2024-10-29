f5026e7877e3f6d03fb45f673c9de1646a8bea56
AngelMarquesGarcia <117300926+AngelMarquesGarcia@users.noreply.github.com>
2024-10-29 18:30:12 +0100

    feat: add basic security configuration
    The files were taken from a previous project, and some were modified to work with Spring Boot 3.
    
    The project in question: https://github.com/CodeURJC-DAW-2023-24/webapp11

--- src/main/java/codeurjc_students/ATRA/model/User.java
+++ src/main/java/codeurjc_students/ATRA/model/User.java
@@ -5,6 +5,9 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @Setter
 @Getter
@@ -16,4 +19,16 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	private String username;
+
+	//<editor-fold desc="private List<String> roles">
+	@Column(name = "role")
+	@ElementCollection(fetch = FetchType.EAGER)
+	@CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+	private List<String> roles;
+	//</editor-fold>
+
+	public String getEncodedPassword() {
+		return "";
+	}
 }
---
+++ src/main/java/codeurjc_students/ATRA/security/RestSecurityConfig.java
@@ -0,0 +1,83 @@
+package codeurjc_students.ATRA.security;
+
+import codeurjc_students.ATRA.security.jwt.JwtRequestFilter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.security.SecureRandom;
+
+@Configuration
+public class RestSecurityConfig {
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Autowired
+	private JwtRequestFilter jwtRequestFilter;
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService)
+				.passwordEncoder(passwordEncoder());
+	}
+
+	//Expose AuthenticationManager as a Bean to be used in other services
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+
+	//@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(10, new SecureRandom());
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+		http
+			.securityMatcher("/api/**")
+			.authorizeHttpRequests(authorize -> authorize
+		
+				// URLs that need authentication to access to it
+				.requestMatchers(HttpMethod.POST, "/api/example/1").hasRole("ADMIN")
+				.requestMatchers(HttpMethod.GET, "/api/example/2").hasRole("USER")
+				.requestMatchers(HttpMethod.GET, "/api/example/3").hasAnyRole("ADMIN", "USER")
+				.requestMatchers(HttpMethod.GET, "/api/example/4").permitAll()
+
+				// Other URLs can be accessed without authentication
+				.anyRequest().permitAll())
+
+			// Disable CSRF protection (it is difficult to implement in REST APIs)
+			.csrf(AbstractHttpConfigurer::disable) //csrf -> csrf.disable()
+
+			// Disable Http Basic Authentication
+			.httpBasic(AbstractHttpConfigurer::disable) //httpBasic -> httpBasic.disable()
+
+			// Disable Form login Authentication
+			.formLogin(AbstractHttpConfigurer::disable) //formLogin -> formLogin.disable()
+
+			// Avoid creating session
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+			// Add JWT Token filter
+			.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+		return http.build();
+	}
+}
