a3cf489e281e53eab8567a7ea57da7a4df3b397e
AngelMarquesGarcia <117300926+AngelMarquesGarcia@users.noreply.github.com>
2024-11-27 15:34:37 +0100

    build: create empty components for mural and home

--- frontend/ATRA/src/app/app.routes.ts
+++ frontend/ATRA/src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
+import { HomeComponent } from './components/home/home.component';
+import { MuralsComponent } from './components/murals/murals.component';
 
 export const routes: Routes = [
   {path: '', component: WelcomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'home', component: RegisterComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'murals', component: MuralsComponent}
 ];
---
+++ frontend/ATRA/src/app/components/murals/murals.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-murals',
+  standalone: true,
+  imports: [],
+  templateUrl: './murals.component.html',
+  styleUrl: './murals.component.css'
+})
+export class MuralsComponent {
+
+}
