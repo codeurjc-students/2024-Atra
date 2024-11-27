7d0b17f7c87f1bf67fac6c20c610ca2fd5fc7bbc
AngelMarquesGarcia <117300926+AngelMarquesGarcia@users.noreply.github.com>
2024-11-27 15:34:16 +0100

    feat: add basic side and top bars. Styling needs work.

--- frontend/ATRA/src/app/app.component.ts
+++ frontend/ATRA/src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterModule, RouterOutlet } from '@angular/router';
 import { RegisterComponent } from "./components/register/register.component";
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ATRA';
+  showSideBar: boolean = true;
+
+  constructor(private router: Router){}
+
+  ngOnInit(){
+    this.router.events.subscribe(
+      (event) => {
+        if (event instanceof NavigationEnd) {
+          console.log(this.router.url)
+          this.showSideBar = this.router.url === "/"
+        }
+      }
+    )
+  }
+
+
 }
