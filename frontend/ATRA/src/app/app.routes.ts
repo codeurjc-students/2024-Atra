a37abb52088e982ef1ade647906afb618ccf5f3b
AngelMarquesGarcia <117300926+AngelMarquesGarcia@users.noreply.github.com>
2024-12-02 10:26:05 +0100

    build: create new comparison and activity components
    Also change their icons

--- frontend/ATRA/src/app/app.routes.ts
+++ frontend/ATRA/src/app/app.routes.ts
@@ -4,11 +4,15 @@ import { RegisterComponent } from './components/register/register.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { HomeComponent } from './components/home/home.component';
 import { MuralsComponent } from './components/murals/murals.component';
+import { ActivityComponent } from './components/activity/activity.component';
+import { ComparisonComponent } from './components/comparison/comparison.component';
 
 export const routes: Routes = [
   {path: '', component: WelcomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'home', component: HomeComponent},
+  {path: 'activity-comparison', component: ComparisonComponent},
+  {path: 'activity-view', component: ActivityComponent},
   {path: 'murals', component: MuralsComponent}
 ];
