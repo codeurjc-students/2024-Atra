b8e1bb9b3bb1917cc0def5cbe9fe3cbc1eb420e6
AngelMarquesGarcia <117300926+AngelMarquesGarcia@users.noreply.github.com>
2025-01-27 19:08:46 +0100

    chore: restructured study page

--- frontend/ATRA/src/app/components/activity/activity.component.ts
+++ frontend/ATRA/src/app/components/activity/activity.component.ts
@@ -1,6 +1,6 @@
 import { ActivityService } from './../../services/activity.service';
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Activity } from '../../models/activity.model';
 import { FormsModule } from '@angular/forms';
@@ -14,16 +14,33 @@ import { NgxChartsModule } from '@swimlane/ngx-charts';
   templateUrl: './activity.component.html',
   styleUrl: './activity.component.css'
 })
-export class ActivityComponent implements OnInit{
+export class ActivityComponent implements OnInit, AfterViewInit{
+  @ViewChild('chartContainer', { static: true }) chartContainer!: ElementRef;
+
   id !: string;
   stats !: {name:string, value:string}[];
   activity !: Activity;
   dataset : {name:string, value:number}[] = [];
   currentMetric : string = "pace";
+  view: [number, number] = [400, 400];
 
+  ngAfterViewInit() {
+    const containerWidth = this.chartContainer.nativeElement.offsetWidth;
+    const containerHeight = this.chartContainer.nativeElement.offsetHeight;
+    if (containerHeight == 0 || containerWidth == 0) {
+       this.view = [800, 375]
+       return
+    }
+    console.log([containerWidth, containerHeight])
+    this.view = [containerWidth*0.8, containerHeight*0.8];
+  }
 
   constructor(private route: ActivatedRoute, private router:Router, private activityService: ActivityService) {
   }
+  resizeChart(): void {
+    this.ngAfterViewInit();
+  }
+
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get("id");
@@ -52,6 +69,11 @@ export class ActivityComponent implements OnInit{
   }
 
 
+  // Update chart size on window resize
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.ngAfterViewInit();
+  }
 
 
 
