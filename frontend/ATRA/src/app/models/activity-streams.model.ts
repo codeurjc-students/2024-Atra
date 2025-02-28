3185a9756fa91c499f656a7de040d1f2e5d8abd8
AngelMarquesGarcia <117300926+AngelMarquesGarcia@users.noreply.github.com>
2025-02-28 13:37:49 +0100

    chore: add elevation_gain as a graphable key

--- frontend/ATRA/src/app/models/activity-streams.model.ts
+++ frontend/ATRA/src/app/models/activity-streams.model.ts
@@ -1,12 +1,13 @@
 export class ActivityStreams {
   static getGraphableKeys(): string[]{
-    return ["altitude", "distance", "heartrate", "cadence", "pace"]
+    return ["altitude", "distance", "heartrate", "cadence", "pace", "elevation_gain"]
   }
 
   time : string[] = []
   distance : string[] = []
   position : string[] = []
   altitude : string[] = []
+  elevation_gain : string[] = []
   heartrate : string[] = []
   cadence : string[] = []
   pace : string[] = []
