codeurjc-students/2024-Atra

commit 52d30ed4362c4c63a6a52014e2d8460b3630a49b

chore: minor fixes/changes to DTOs

src/main/java/codeurjc_students/ATRA/dto/ActivityDTO.java
@@ -25,7 +25,7 @@ public class ActivityDTO implements ActivityDtoInterface {
 	private String name;
 	private String type;
 	private Instant startTime;
-	private Long user;
+	private NamedId user; //should check this doesn't break anything
 	private NamedId route;
 	private List<DataPoint> dataPoints; // I think this can be deleted
 	private Map<String, List<String>> streams;
@@ -36,37 +36,33 @@ public class ActivityDTO implements ActivityDtoInterface {
 		name = activity.getName();
 		type = activity.getType();
 		startTime = activity.getStartTime();
-		user = activity.getUser().getId();
+		user = activity.getUser();
 		dataPoints = activity.getDataPoints();
 		setUpStreams(activity.getDataPoints());
 		summary = new ActivitySummary(this);
-		if (activity.getRoute() == null) {
-			route = null;
-		} else {
-			throw new RuntimeException("ActivityDTO(Activity a) called with activity.getRoute()!=null");
-		}
-
-
+		route = activity.getRoute() != null ? new BasicNamedId(activity.getRoute().getId(), activity.getRoute().getName()) : null;
 	}
 
+	//these may not be needed (since activity holds its route now)
 	public ActivityDTO(Activity activity, NamedId routeIdAndName) {
 		id = activity.getId();
 		name = activity.getName();
 		type = activity.getType();
 		startTime = activity.getStartTime();
-		user = activity.getUser().getId();
+		user = activity.getUser();
 		route = routeIdAndName;
 		dataPoints = activity.getDataPoints();
 		setUpStreams(activity.getDataPoints());
 		summary = new ActivitySummary(this);
 	}
 
+	//these may not be needed.
 	public ActivityDTO(Activity activity, Route route) {
 		this.id = activity.getId();
 		this.name = activity.getName();
 		this.type = activity.getType();
 		this.startTime = activity.getStartTime();
-		this.user = activity.getUser().getId();
+		user = activity.getUser();
 		this.route = new BasicNamedId(route);
 		this.dataPoints = activity.getDataPoints();
 		setUpStreams(activity.getDataPoints());

frontend/ATRA/src/app/models/mural.model.ts
@@ -7,10 +7,11 @@ export interface Mural {
     name: string;
     description: string;
 
-    owner: User;
-    members: User[];
+    owner: {name:string;id:number};
+    members: {name:string;id:number}[];
 
-    activities: Activity[];
+    activities: {name:string;id:number}[];
+    routes: {name:string;id:number}[];
 
     thumbnailUrl:string;
     thumbnail: Blob;

frontend/ATRA/src/app/models/user.model.ts
@@ -8,4 +8,8 @@ export interface User {
 
     roles: string[];
 
+    activities?: { name: string; id: number }[];
+    routes?: { name: string; id: number }[];
+    murals?: { name: string; id: number }[];
+
 }

src/main/java/codeurjc_students/ATRA/dto/MuralDTO.java
@@ -43,10 +43,10 @@ public class MuralDTO {
 			this.members.add(new BasicNamedId(user.getId(), user.getName()));
 		});
 		mural.getActivities().forEach(activity -> {
-			this.members.add(new BasicNamedId(activity.getId(), activity.getName()));
+			this.activities.add(new BasicNamedId(activity.getId(), activity.getName()));
 		});
 		mural.getRoutes().forEach(route -> {
-			this.members.add(new BasicNamedId(route.getId(), route.getName()));
+			this.routes.add(new BasicNamedId(route.getId(), route.getName()));
 		});
 	}
 }

commit c2ee3be1e1454d3812b47b9bfdf2906911c829da

fix: a bug introduced in commit 52d30ed4362c4c63a6a52014e2d8460b3630a49b
Turns out, if you put a User into a NamedId field, the whole User is sent to the frontend. Who knew. Maybe having User extend BasicNamedId would fix it, but this is jus easier honestly

src/main/java/codeurjc_students/ATRA/dto/ActivityDTO.java
@@ -36,7 +36,7 @@ public class ActivityDTO implements ActivityDtoInterface {
 		name = activity.getName();
 		type = activity.getType();
 		startTime = activity.getStartTime();
-		user = activity.getUser();
+		user = new BasicNamedId(activity.getUser());
 		dataPoints = activity.getDataPoints();
 		setUpStreams(activity.getDataPoints());
 		summary = new ActivitySummary(this);
@@ -49,7 +49,7 @@ public class ActivityDTO implements ActivityDtoInterface {
 		name = activity.getName();
 		type = activity.getType();
 		startTime = activity.getStartTime();
-		user = activity.getUser();
+		user = new BasicNamedId(activity.getUser());
 		route = routeIdAndName;
 		dataPoints = activity.getDataPoints();
 		setUpStreams(activity.getDataPoints());
@@ -62,7 +62,7 @@ public class ActivityDTO implements ActivityDtoInterface {
 		this.name = activity.getName();
 		this.type = activity.getType();
 		this.startTime = activity.getStartTime();
-		user = activity.getUser();
+		this.user = new BasicNamedId(activity.getUser());
 		this.route = new BasicNamedId(route);
 		this.dataPoints = activity.getDataPoints();
 		setUpStreams(activity.getDataPoints());