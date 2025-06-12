e1cf5418f5c558f25ecb7c1c15916c953dc78d0a
AngelMarquesGarcia <117300926+AngelMarquesGarcia@users.noreply.github.com>
2025-06-12 17:45:20 +0200

    fix: immediate logout on 401
    Sometimes the backend crashes when decoding the JWT. This is interpreted as a failed authentication, and so 401 is returned. When a 401 was returned, the front immediately logged the user out. This is a hassle, as you're constantly being wrongfully logged out.
    
    This has been solved by only logging the user out once they dismiss the alert informing them they've been logged out, and the alert's content has been modified to offer the option of reloading the page.

--- frontend/ATRA/src/app/services/auth-interceptor.service.ts
+++ frontend/ATRA/src/app/services/auth-interceptor.service.ts
@@ -21,8 +21,8 @@ export class AuthInterceptorService {
     return next.handle(req).pipe(
       catchError(err => {
         if (err.status === 401) {
-          this.authService.logout(); // maybe clear tokens, etc.
-          this.alertService.alert("You are not logged in, or your credentials have expired. You'll be redirected to the login page.", "Not logged in",  ()=>this.router.navigate(['/']));
+          //this.authService.logout(); // maybe clear tokens, etc.
+          this.alertService.alert("You are not logged in, or your credentials have expired. \n If you think this is a mistake, you can reload the page. If it was a mistake you'll be allowed to continue.\n Otherwise, click 'ok', and you'll be redirected to the login page.", "Not logged in",  ()=>{this.authService.logout();this.router.navigate(['/'])});
         } else if (err.status === 403) {
           this.alertService.alert('You do not have permission to access this resource.', '403 Forbidden'); //Should be toast
         }
