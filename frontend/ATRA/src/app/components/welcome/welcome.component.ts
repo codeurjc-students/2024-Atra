576c31376437d4b16aaed203207d6659d8925407
AngelMarquesGarcia <117300926+AngelMarquesGarcia@users.noreply.github.com>
2025-06-20 17:23:17 +0200

    refactor: change layout of welcome component
    Delete anon-init and user-init component. They had little reason to exist, they were assimilated into the welcome component

--- frontend/ATRA/src/app/components/welcome/welcome.component.ts
+++ frontend/ATRA/src/app/components/welcome/welcome.component.ts
@@ -1,15 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { UserInitComponent } from "../user-init/user-init.component";
-import { AnonInitComponent } from "../anon-init/anon-init.component";
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { RegisterComponent } from '../register/register.component';
+import { LoginComponent } from '../login/login.component';
+import { ActivityService } from '../../services/activity.service';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [CommonModule, UserInitComponent, AnonInitComponent],
+  imports: [CommonModule],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.scss'
 })
 export class WelcomeComponent {
+
+  constructor(private modalService: NgbModal, private activityService:ActivityService){}
+
+  onFileSelected(event: Event){
+    this.activityService.uploadActivity(event)
+  }
+
+  openRegisterModal() {
+    const modalRef = this.modalService.open(RegisterComponent, { centered:true });
+  }
+  openLoginModal() {
+    const modalRef = this.modalService.open(LoginComponent, { centered:true });
+  }
 
 }
