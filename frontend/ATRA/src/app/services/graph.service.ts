374e0dd5828cb6bb0b7bf13938654498435337c8
AngelMarquesGarcia <117300926+AngelMarquesGarcia@users.noreply.github.com>
2025-08-31 14:48:14 +0200

    refactor: fix tabs in graph service

--- frontend/ATRA/src/app/services/graph.service.ts
+++ frontend/ATRA/src/app/services/graph.service.ts
@@ -60,42 +60,42 @@ export class GraphService {
   constructor(private activityService: ActivityService) { }
 
   getAvg(data:number[]): number{
-  let total = 0
-  for (let d of data){
-    total += d
-  }
+    let total = 0
+    for (let d of data){
+      total += d
+    }
 
-  return total/data.length
+    return total/data.length
   }
 
   getDeviation(data:number[], includedAvg:boolean=false, avg:number=0){
-  if (!includedAvg) {
-    avg = this.getAvg(data)
-  }
+    if (!includedAvg) {
+      avg = this.getAvg(data)
+    }
 
-  let total = 0
-  for (let d of data) {
-    total += Math.pow(d-avg, 2)
-  }
-  return Math.sqrt(total/(data.length-1))
+    let total = 0
+    for (let d of data) {
+      total += Math.pow(d-avg, 2)
+    }
+    return Math.sqrt(total/(data.length-1))
   }
 
 
   getGraphData(metric: string, activity: Activity, xAxis: string, partitions:number=-1): { name: string, value: number; }[] {
-  var metricData:number[];
-  const time = activity.streams.time;
-  const distance = activity.streams.distance
+    var metricData:number[];
+    const time = activity.streams.time;
+    const distance = activity.streams.distance
 
-  if (!(Object.keys(new ActivityStreams())).includes(metric)) {
-      throw new Error(`Given metric '${metric}' is not a key of ActivityStreams`)
-  }
-  metricData = activity.streams[metric as keyof ActivityStreams].map(x => parseFloat(x))
-  if (xAxis=="distribution"){
-      return this.distributionData(metricData, partitions)
-  }
-  const processedData: {name: string; value: number;}[] = []
-  const startTime = new Date(time[0]);
-  for (var i=0;i<metricData.length;i++) {
+    if (!(Object.keys(new ActivityStreams())).includes(metric)) {
+        throw new Error(`Given metric '${metric}' is not a key of ActivityStreams`)
+    }
+    metricData = activity.streams[metric as keyof ActivityStreams].map(x => parseFloat(x))
+    if (xAxis=="distribution"){
+        return this.distributionData(metricData, partitions)
+    }
+    const processedData: {name: string; value: number;}[] = []
+    const startTime = new Date(time[0]);
+    for (var i=0;i<metricData.length;i++) {
       const currentTime = new Date(time[i])
 
       switch(xAxis) {
@@ -103,16 +103,13 @@ export class GraphService {
         case "timeOfDay": processedData.push({name:currentTime.getHours().toString()+":"+currentTime.getMinutes().toString()+":"+currentTime.getSeconds().toString(),value:metricData[i]}); break;
         case "totalDistance": processedData.push({name:parseFloat(distance[i]).toFixed(2),value:metricData[i]}); break;
         default: {
-          console.log("xAxisRepresents has a bad value. Defaulting to timeElapsed")
+          console.log("(GraphService) xAxisRepresents has a bad value. Defaulting to timeElapsed")
           processedData.push({name:this.activityService.getTime(currentTime, startTime),value:metricData[i]})
         }
       }
+    }
+    return processedData //[{ name: "1", value: 2 }, { name: "2", value: 2 },{ name: "3", value: 3 }]
   }
-  return processedData //[{ name: "1", value: 2 }, { name: "2", value: 2 },{ name: "3", value: 3 }]
-  }
-
-
-
 
   distributionData(metricData: number[], partitions: number) {
     const lowest = Math.min(...metricData)
@@ -122,13 +119,7 @@ export class GraphService {
     const arrays: number[][]= Array.from({ length: partitions }, () => []); // n empty arrays
     const processedData: {name: string; value: number;}[] = []
 
-    console.log("Lowest:", lowest);
-    console.log("Highest:", highest);
-    console.log("Difference:", diff);
-    console.log("Partition Size:", partSize);
-
     for (const n of metricData){
-
       if (Math.floor((n-lowest)/partSize)>=partitions){
         console.log("exception")
         arrays[partitions-1].push(n)
@@ -140,17 +131,6 @@ export class GraphService {
     for (let i = 0; i < partitions; i++) {
       processedData.push({name: (lowest+(i*partSize)).toFixed(1)+"-"+(lowest+(i+1)*partSize).toFixed(1), value: arrays[i].length})
     }
-    console.log(processedData)
-
-    //c++;
-    //container.push(parseFloat(metricData[i]))
-    //if (c==p || i==metricData.length-1){
-    //  console.log("We in")
-    //  processedData.push({name:i.toString(),value:this.avg(container)})
-    //  c=0
-    //  container=[]
-    //};
-
     return processedData
   }
 
